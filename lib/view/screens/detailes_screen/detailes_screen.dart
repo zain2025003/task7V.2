7fc675bbc627f5dd8bf2afa878e057c1bf4c9b14
Zain <zainateah.focal.x@gmail.com>
2024-10-30 19:26:24 +0300

    secondUpdate

--- lib/view/screens/detailes_screen/detailes_screen.dart
+++ lib/view/screens/detailes_screen/detailes_screen.dart
@@ -26,7 +26,7 @@ class DetailesScreen extends StatelessWidget {
               DetailesScreenCustomVideoDescription(),
               DetailesScreenCustomUserInformation(),
               DetailesScreenCustomFeedback()
-              //ttttrrreerr
+              //ttttrrreerrrr
             ],
           ),
         ),
